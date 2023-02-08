a64ef76fc37035ee013fd5a7f0e257cd9a2f2b22
Sanyam Saini <sanyamsaini51@gmail.com>
2023-02-08 17:47:56 +0530

    ViewModel + Binding + LiveData

--- app/src/main/java/com/sanyam/binding/example/livedata/MainActivity.kt
+++ app/src/main/java/com/sanyam/binding/example/livedata/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.sanyam.binding.example.livedata
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.sanyam.binding.example.livedata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var mainViewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        binding.mainViewModel = mainViewModel
+        binding.lifecycleOwner = this
     }
 }
---
+++ app/src/main/java/com/sanyam/binding/example/livedata/MainViewModel.kt
@@ -0,0 +1,14 @@
+package com.sanyam.binding.example.livedata
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    val quoteLiveData = MutableLiveData<String>("What you give is what you get.")
+
+    fun updateQuote() {
+        quoteLiveData.value = "You'll see it when you believe it."
+    }
+
+}
